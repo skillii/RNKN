8f1d290a96988b5e1dfb7c128b7db604885331b1
Thomas Ebner <t.ebner@student.tugraz.at>
2011-04-01 23:33:39 +0200

    added my changes

--- org/iaik/net/layers/DefaultInternetLayer.java
+++ org/iaik/net/layers/DefaultInternetLayer.java
@@ -38,15 +38,19 @@ import org.iaik.net.Network;
 import org.iaik.net.StackParameters;
 import org.iaik.net.datatypes.interfaces.ARPTable;
 import org.iaik.net.exceptions.NetworkException;
+import org.iaik.net.exceptions.PacketParsingException;
 import org.iaik.net.factories.LinkLayerFactory;
 import org.iaik.net.interfaces.InternetLayer;
 import org.iaik.net.interfaces.PhysicalSender;
 import org.iaik.net.packets.ARPPacket;
 import org.iaik.net.packets.EthernetPacket;
+import org.iaik.net.packets.ICMPPacket;
 import org.iaik.net.packets.IPPacket;
 import org.iaik.net.packets.Packet;
 import org.iaik.net.utils.NetUtils;
 import org.iaik.net.utils.NetworkBuffer;
+
+import sun.security.pkcs.ParsingException;
 
 /**
  * 
@@ -233,7 +237,8 @@ public class DefaultInternetLayer extends Thread implements InternetLayer {
 			 */
 			if (packet.getTimeout() == 0 || packet.getTimeout() + StackParameters.ARP_TIMEOUT > System.currentTimeMillis()) {
 
-				String destinationMACAddress = resolveAddress(((IPPacket) packet).getDestinationAddress());
+				//String destinationMACAddress = resolveAddress(((IPPacket) packet).getDestinationAddress());
+				String destinationMACAddress = "0A:00:27:00:00:00";
 
 				if (!(destinationMACAddress instanceof String)) {
 					/*
@@ -321,7 +326,33 @@ public class DefaultInternetLayer extends Thread implements InternetLayer {
 	 *            The received IP packet.
 	 */
 	public void processIP(IPPacket packet) {
-
+		System.out.println("Received a packet:");
+		if(packet.getProtocol() == IPPacket.ICMP_PROTOCOL) {
+			System.out.println("    Received an ICMP packet!!!");
+			
+			ICMPPacket icmp;
+			
+			try
+			{
+				icmp = ICMPPacket.createICMPPacket(packet);
+				System.out.print(icmp.getInfo());
+			}
+			catch(PacketParsingException ex)
+			{
+				System.out.println("ICMP packet contained errors:" + ex.getMessage());
+				return;
+			}
+			
+			switch(icmp.getType())
+			{
+				case ICMPPacket.ECHO_REQUEST:
+					ICMPPacket icmpreply = ICMPPacket.createICMPPacket(ICMPPacket.ECHO_REPLY, (short)1);
+					IPPacket ipreply = IPPacket.createDefaultIPPacket(IPPacket.ICMP_PROTOCOL, identification, packet.getDestinationAddress(), packet.getSourceAddress(), icmpreply.getPacket());
+					send(ipreply);
+					break;
+			}
+			
+		}
 	}
 
 	/**
@@ -357,7 +388,6 @@ public class DefaultInternetLayer extends Thread implements InternetLayer {
 	 * @return The resolved MAC address of the specified IP address.
 	 */
 	private String resolveAddress(String remoteIP) {
-
 		return null;
 	}
 
