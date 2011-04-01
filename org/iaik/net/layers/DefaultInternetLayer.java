715ed9d8bd75a6dee96faa46528037ca3996d8b5
Thomas Ebner <t.ebner@student.tugraz.at>
2011-04-01 23:25:49 +0200

    bla

--- org/iaik/net/layers/DefaultInternetLayer.java
+++ org/iaik/net/layers/DefaultInternetLayer.java
@@ -44,10 +44,13 @@ import org.iaik.net.interfaces.InternetLayer;
 import org.iaik.net.interfaces.PhysicalSender;
 import org.iaik.net.packets.ARPPacket;
 import org.iaik.net.packets.EthernetPacket;
+import org.iaik.net.packets.ICMPPacket;
 import org.iaik.net.packets.IPPacket;
 import org.iaik.net.packets.Packet;
 import org.iaik.net.utils.NetUtils;
 import org.iaik.net.utils.NetworkBuffer;
+
+import sun.security.pkcs.ParsingException;
 
 /**
  * 
@@ -101,6 +104,7 @@ public class DefaultInternetLayer extends Thread implements InternetLayer {
 		sender = LinkLayerFactory.getInstance().getSender();
 
 		while (running) {
+
 			Packet receivedPacket = receiveBuffer.getNextPacket();
 
 			if (receivedPacket instanceof Packet) {
@@ -233,7 +237,8 @@ public class DefaultInternetLayer extends Thread implements InternetLayer {
 			 */
 			if (packet.getTimeout() == 0 || packet.getTimeout() + StackParameters.ARP_TIMEOUT > System.currentTimeMillis()) {
 
-				String destinationMACAddress = resolveAddress(((IPPacket) packet).getDestinationAddress());
+				//String destinationMACAddress = resolveAddress(((IPPacket) packet).getDestinationAddress());
+				String destinationMACAddress = "0A:00:27:00:00:00";
 
 				if (!(destinationMACAddress instanceof String)) {
 					/*
@@ -308,24 +313,8 @@ public class DefaultInternetLayer extends Thread implements InternetLayer {
 	 *            The received ARP request.
 	 */
 	public void processARP(ARPPacket packet) {
-		
-		System.out.println("Begin of process ARP method\n");
-		Properties pts = this.getProperties();
-	
-
-		ARPPacket reply = ARPPacket.createARPPacket(ARPPacket.ARP_REPLY,
-					                                pts.getProperty("mac-address") ,
-                                                    pts.getProperty("ip-address"), 
-                                                    packet.getSenderHardwareAddress(), 
-                                                    packet.getSenderProtocolAddress());
-
-	    System.out.println("ARP REPLY SENT\n");
-	    
-	    this.send(reply);
-	
-		
-		
 	}
+
 	/**
 	 * Processes an received IP packet. If the IP packet contains an ICMP, IGMP
 	 * or OSPF packet they are processed directly in this function. A response
@@ -337,7 +326,33 @@ public class DefaultInternetLayer extends Thread implements InternetLayer {
 	 *            The received IP packet.
 	 */
 	public void processIP(IPPacket packet) {
-
+		System.out.println("Received a packet:");
+		if(packet.getProtocol() == IPPacket.ICMP_PROTOCOL) {
+			System.out.println("    Received an ICMP packet!!!");
+			
+			ICMPPacket icmp;
+			
+			try
+			{
+				icmp = ICMPPacket.createICMPPacket(packet);
+				System.out.print(icmp.getInfo());
+			}
+			catch(PacketParsingException ex)
+			{
+				System.out.println("ICMP packet contained errors:" + ex.getMessage());
+				return;
+			}
+			
+			switch(icmp.getType())
+			{
+				case ICMPPacket.ECHO_REQUEST:
+					ICMPPacket icmpreply = ICMPPacket.createICMPPacket(ICMPPacket.ECHO_REPLY, (short)1);
+					IPPacket ipreply = IPPacket.createDefaultIPPacket(IPPacket.ICMP_PROTOCOL, identification, packet.getDestinationAddress(), packet.getSourceAddress(), icmpreply.getPacket());
+					send(ipreply);
+					break;
+			}
+			
+		}
 	}
 
 	/**
@@ -373,7 +388,6 @@ public class DefaultInternetLayer extends Thread implements InternetLayer {
 	 * @return The resolved MAC address of the specified IP address.
 	 */
 	private String resolveAddress(String remoteIP) {
-
 		return null;
 	}
 
--- org/iaik/net/packets/ICMPPacket.java
+++ org/iaik/net/packets/ICMPPacket.java
@@ -30,10 +30,14 @@
 
 package org.iaik.net.packets;
 
+import java.util.ArrayList;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.iaik.net.exceptions.PacketParsingException;
 import org.iaik.net.utils.NetUtils;
+
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 /**
  * 
@@ -123,16 +127,44 @@ public class ICMPPacket implements Packet {
 	private long timeout = 0;
 
 	private Log log;
+	
+	private byte[] payload;
 
 	private ICMPPacket(byte[] packet) throws PacketParsingException {
 		log = LogFactory.getLog(this.getClass());
+		
+		final int typeOffset = 0;
+		final int codeOffset = 1;
+		final int checkSumOffset = 2;
+		final int identifierOffset = 4;
+		final int sequenceNumberOffset = 6;
+		final int payloadOffset = 8;
+		
+
+		if(packet.length - payloadOffset < 0)
+			throw new PacketParsingException("packet too small???");
+		
+		type = packet[typeOffset];
+		code = packet[codeOffset];
+		identifier = NetUtils.bytesToShort(packet, identifierOffset);
+		sequenceNumber = NetUtils.bytesToShort(packet, sequenceNumberOffset);
+		checksum = NetUtils.bytesToShort(packet, checkSumOffset);
+		
+		
+		payload = new byte[packet.length - payloadOffset];
+		System.arraycopy(packet, payloadOffset, payload, 0, packet.length - payloadOffset);
 	}
 
 	private ICMPPacket(byte type, byte code, short identifier, short sequenceNumber, byte[] payload) {
+		this.type = type;
+		this.code = code;
+		this.identifier = identifier;
+		this.sequenceNumber = sequenceNumber;
+		this.payload = payload;
 	}
 
 	public static ICMPPacket createICMPPacket(IPPacket packet) throws PacketParsingException {
-		return null;
+		return new ICMPPacket(packet.getPayload());
 	}
 
 	public static ICMPPacket createICMPPacket(byte type, byte code, short identifier, short sequenceNumber, byte[] payload) {
@@ -140,7 +172,8 @@ public class ICMPPacket implements Packet {
 	}
 
 	public static ICMPPacket createICMPPacket(byte type, short sequenceNumber) {
-		return null;
+		//TODO: code, identifier?
+		return new ICMPPacket(type, (byte)0, (short)0, sequenceNumber, DEFAULT_PAYLOAD);
 	}
 
 	public short getChecksum() {
@@ -172,7 +205,28 @@ public class ICMPPacket implements Packet {
 	}
 
 	public byte[] getPacket() {
-		return null;
+		final int typeOffset = 0;
+		final int codeOffset = 1;
+		final int checkSumOffset = 2;
+		final int identifierOffset = 4;
+		final int sequenceNumberOffset = 6;
+		final int payloadOffset = 8;
+		
+		byte[] pkg = new byte[payload.length + 8];
+		
+		pkg[typeOffset] = type;
+		pkg[codeOffset] = code;
+		
+		pkg[checkSumOffset] = 0;
+		pkg[checkSumOffset+1] = 0;
+		
+		NetUtils.insertData(pkg, NetUtils.shortToBytes(identifier), identifierOffset);
+		NetUtils.insertData(pkg, NetUtils.shortToBytes(sequenceNumber), sequenceNumberOffset);
+		NetUtils.insertData(pkg, payload, payloadOffset);
+		
+		//TODO: checksum calculation
+		
+		return pkg;
 	}
 
 	public String getInfo() {
