a1b80aaacd3d86ee33266ef74091d8171abbece5
Christoph Bichler <christoph.bichler@student.tugraz.at>
2011-06-14 21:29:49 +0200

    added and implemented data packets and ack packets

--- org/iaik/net/packets/rudp/RUDP_ACKPacket.java
+++ org/iaik/net/packets/rudp/RUDP_ACKPacket.java
@@ -1,17 +1,78 @@
 package org.iaik.net.packets.rudp;
 
+import org.iaik.net.exceptions.PacketParsingException;
+import org.iaik.net.utils.NetUtils;
+
 public class RUDP_ACKPacket extends RUDPPacket {
 
+	byte advertised_window_size;
+	
+	private RUDP_ACKPacket(byte[] packet) throws PacketParsingException
+	{
+		  
+		  this.ack = true;
+		  this.syn = false;
+		  this.eak = false;
+		  this.rst = false;
+		  this.nul = false;
+
+		  this.packet_length = packet[1];
+		  this.dest_port = NetUtils.bytesToShort(packet, 2);
+	      this.src_port = NetUtils.bytesToShort(packet, 4);
+		  
+	      this.seq_num = packet[6];
+	      this.ack_num = packet[7];
+
+	      this.advertised_window_size = packet[8];
+	      this.checksum = NetUtils.bytesToShort(packet, 9);
+	}
+	
+	public static RUDP_ACKPacket createACKPacket(byte[] packet) throws PacketParsingException
+	{
+      return new RUDP_ACKPacket(packet);		
+	}
+	
 	@Override
 	public String getInfo() {
-		// TODO Auto-generated method stub
-		return null;
+		StringBuffer info = new StringBuffer();
+		info.append("RUDP_ACK packet info:\n");
+		info.append("__________________________________________________________\n");
+		info.append("Destination Port        : " + NetUtils.toInt(this.dest_port) + "\n");
+		info.append("Source Port             : " + NetUtils.toInt(this.src_port) + "\n");
+		info.append("Checksum              : " + NetUtils.toInt(checksum) + "\n");
+		info.append("Sequence number       : " + NetUtils.toInt(this.seq_num) + "\n");
+		info.append("Ack number            : " + NetUtils.toInt(this.ack_num) + "\n");
+		info.append("__________________________________________________________\n");
+
+		return info.toString();
 	}
 
 	@Override
 	public byte[] getPacket() {
-		// TODO Auto-generated method stub
-		return null;
+        byte[] pkg = new byte[this.packet_length];
+		
+		int header_identifier = 64;
+		
+		NetUtils.insertData(pkg, NetUtils.intToBytes(header_identifier), 0);
+        pkg[1] = this.packet_length;
+        NetUtils.insertData(pkg, NetUtils.shortToBytes(this.dest_port), 2);
+        NetUtils.insertData(pkg, NetUtils.shortToBytes(this.src_port), 4);
+        pkg[6] = this.seq_num;
+        pkg[7] = this.ack_num;
+
+        
+        //Set Checksum 0 for now
+        pkg[8] = 0;
+        pkg[9] = 0;
+
+        
+        short calc_checksum = NetUtils.calcIPChecksum(pkg, 0, pkg.length);
+        
+		this.checksum = calc_checksum;
+		
+		NetUtils.insertData(pkg, NetUtils.shortToBytes(this.checksum), 22);
+		
+		return pkg;
 	}
 
 	@Override
