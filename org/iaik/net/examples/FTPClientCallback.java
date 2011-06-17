0b37094c861a80862f321b8d5695f05b501881a5
Christoph Bichler <christoph.bichler@student.tugraz.at>
2011-06-17 18:33:45 +0200

    work on ftp

---
+++ org/iaik/net/examples/FTPClientCallback.java
@@ -0,0 +1,20 @@
+package org.iaik.net.examples;
+
+import org.iaik.net.interfaces.RUDPClientCallback;
+
+public class FTPClientCallback implements RUDPClientCallback {
+
+	@Override
+	public void ConnectionClosed() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void DataReceived() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
